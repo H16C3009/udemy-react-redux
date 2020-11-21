cd274320a9603495b2a49c05817b6326815faa4a
yoshi <yoshiaki11205656@gmail.com>
2020-11-21 14:46:38 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import React, { Component } from 'react';
 
-
-function App() {
-  return (
-    <div>
-      <h1>
-        Hello World !
-      </h1>
-    </div>
-  );
+class App extends Component{
+  render() {
+    //const greeting = "Hello Yoshi";
+    //const dom = <h1 className="foo">{ greeting }</h1>;
+    //return dom;
+    return (
+      //<div>
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={ () => {console.log("I am clicked.")}} /> 
+      </React.Fragment>
+      //</div>
+    )
+  }
 }
 
 export default App;
